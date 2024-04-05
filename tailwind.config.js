15ba78d1af23e063c17b931df27f6f5d624e20c7
Eastyn <eastynwork@gmail.com>
2024-04-05 22:50:36 +0900

    add nurinu's photo

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./tailwind/*.{html}"],
+	theme: {
+		extend: {},
+		fontFamily: {
+			Ubuntu: ["Ubuntu"],
+		},
+	},
+	plugins: [],
+};
